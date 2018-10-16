08b31e3a3e471e12f3f7effbec6ade5d04ded7e4
rufunt <jazzoo@mail.ru>
2018-10-17 01:17:36 +0300

    Multiple data

---
+++ 02/main.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_html(url):
+	r = requests.get(url)
+	return r.text
+
+def refined(s):
+	r = s.split(' ')[0]	
+	
+
+def get_data(html):
+	soup = BeautifulSoup(html, 'lxml')
+	popular = soup.find_all('section')[1]
+	plagins = popular.find_all('article')
+
+	for plugin in plugins:
+		name = plugin.find('h2').text
+		url = plugin.find('h2').find('a').get('href')
+		rating = plugin.find('span', class_='rating-count').find('a').text
+
+
+	#return plagins
+	
+
+def main():
+	url = 'https://wordpress.org/plugins/'
+	print(get_data(get_html(url)))
+	
+
+
+if __name__ == '__main__':
+	main()
