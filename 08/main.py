e6642855f63ac71ba17bda5ee2016250811aa187
rufunt <jazzoo@mail.ru>
2018-10-25 21:00:06 +0300

    Prepare for work

--- 08/main.py
+++ 08/main.py
@@ -1,11 +1,21 @@
 import requests
+import csv
 
 def get_html(url):
-	r = 
+	r = requests.get(url)
+	return r.text
+
+def write_csv(data):
+	with open('website.csv', 'a') as f:
+		order = []	
+		writer = csv.DictWriter(f, fieldnames=order)
+		writer.writerow(data)
 
 
 def main():
-	pass
+	url = 'https://www.liveinternet.ru/rating/ru//today.tsv?page=2'
+	data = get_html(url)
+	print(data)
 
 
 if __name__ == '__main__':
