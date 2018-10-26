2dbe080a1c7490bbcdf6c9fd01f58e1b2cb6e883
rufunt <jazzoo@mail.ru>
2018-10-26 20:26:22 +0300

    Update scipt for ajax pages

--- 08/main.py
+++ 08/main.py
@@ -7,15 +7,34 @@ def get_html(url):
 
 def write_csv(data):
 	with open('website.csv', 'a') as f:
-		order = []	
+		order = ['name','url', 'description', 'traffic', 'percent']	
 		writer = csv.DictWriter(f, fieldnames=order)
 		writer.writerow(data)
 
 
 def main():
-	url = 'https://www.liveinternet.ru/rating/ru//today.tsv?page=2'
-	data = get_html(url)
-	print(data)
+
+	for i in range(0, 5000)
+		url = 'https://www.liveinternet.ru/rating/ru//today.tsv?page={}'.format(str(i))
+		response = get_html(url)
+		data = response.strip().split('\n')[1:]
+
+		for row in data:
+			columns = row.strip().split('\t')
+			name = columns[0]
+			url = columns[1]
+			description = columns[2]
+			traffic = columns[3]
+			percent = columns[4]
+
+			data = {'name': name,
+					'url': url,
+					'description': description,
+					'traffic': traffic,
+					'percent': persent}
+
+			write_csv(data)		
+
 
 
 if __name__ == '__main__':
